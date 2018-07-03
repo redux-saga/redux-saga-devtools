44156a94811b1afd53697b87d27801313d165035
Alex Bettadapur <alebet@microsoft.com>
2018-07-03 11:58:19 -0700

    Alpha, add all and set context

--- src/components/Effect/Effect.js
+++ src/components/Effect/Effect.js
@@ -37,21 +37,24 @@ class Effect extends React.Component {
       )
     }
 
-    else if ((data = asEffect.take(effect.effect))) {
+    // ACTION CHANNEL
+    else if ((data = asEffect.actionChannel(effect.effect))) {
       nodes = nodes.concat(
-        renderEffectType('take'),
-        <SagaValue value={data.pattern || data.channel} isIdentifier={true} />,
+        renderEffectType('actionChannel'),
+        <SagaValue value={data.action} isIdentifier={true} />,
         this.renderResult(status, result, error, winner)
       )
     }
 
-    else if ((data = asEffect.put(effect.effect))) {
+    // ALL
+    else if (data = asEffect.all(effect.effect)) {
       nodes = nodes.concat(
-        renderEffectType('put'),
-        <SagaValue value={data.channel || data.action} label={data.action.type} isIdentifier={true} />
+        renderEffectType('all'),
+        this.renderResult(status, result, error, winner)
       )
     }
 
+    // CALL
     else if ((data = asEffect.call(effect.effect))) {
       nodes = nodes.concat(
         renderEffectType('call'),
@@ -60,6 +63,15 @@ class Effect extends React.Component {
       )
     }
 
+    // CANCEL
+    else if ((data = asEffect.cancel(effect.effect))) {
+      nodes = nodes.concat(
+        renderEffectType('cancel'),
+        <SagaValue value={data} isIdentifier={true} label={data.name} />,
+      )
+    }
+
+    // CPS
     else if ((data = asEffect.cps(effect.effect))) {
       nodes = nodes.concat(
         renderEffectType('cps'),
@@ -68,6 +80,16 @@ class Effect extends React.Component {
       )
     }
 
+    // FLUSH
+    else if ((data = asEffect.flush(effect.effect))) {
+      nodes = nodes.concat(
+        renderEffectType('flush'),
+        renderFuncCall(data),
+        this.renderResult(status, result, error, winner)
+      )
+    }
+
+    // FORK
     else if ((data = asEffect.fork(effect.effect))) {
       nodes = nodes.concat(
         renderEffectType('fork'),
@@ -75,6 +97,8 @@ class Effect extends React.Component {
         this.renderResult(status, result, error, winner)
       )
     }
+
+    // GET CONTEXT
     else if ((data = asEffect.getContext(effect.effect))) {
       nodes = nodes.concat(
         renderEffectType('getContext'),
@@ -82,6 +106,8 @@ class Effect extends React.Component {
         this.renderResult(status, result, error, winner)
       )
     }
+
+    // JOIN
     else if ((data = asEffect.join(effect.effect))) {
       nodes = nodes.concat(
         renderEffectType('join'),
@@ -90,10 +116,45 @@ class Effect extends React.Component {
       )
     }
 
-    else if ((data = asEffect.cancel(effect.effect))) {
+    // PUT
+    else if ((data = asEffect.put(effect.effect))) {
       nodes = nodes.concat(
-        renderEffectType('cancel'),
-        <SagaValue value={data} isIdentifier={true} label={data.name} />,
+        renderEffectType('put'),
+        <SagaValue value={data.channel || data.action} label={data.action.type} isIdentifier={true} />
+      )
+    }
+
+    // RACE
+    else if ((data = asEffect.race(effect.effect))) {
+      nodes = nodes.concat(
+        renderEffectType('race'),
+        this.renderResult(status, result, error, winner)
+      )
+    }
+
+    // SELECT
+    else if ((data = asEffect.select(effect.effect))) {
+      nodes = nodes.concat(
+        renderEffectType('select'),
+        renderFuncCall(data.selector, data.args),
+        this.renderResult(status, result, error, winner)
+      )
+    }
+
+    // SET CONTEXT
+    else if ((data = asEffect.setContext(effect.effect))) {
+      nodes = nodes.concat(
+        renderEffectType('setContext'),
+        renderFuncCall({ name: "" }, [data])
+      )
+    }
+
+    // TAKE
+    else if ((data = asEffect.take(effect.effect))) {
+      nodes = nodes.concat(
+        renderEffectType('take'),
+        <SagaValue value={data.pattern || data.channel} isIdentifier={true} />,
+        this.renderResult(status, result, error, winner)
       )
     }
 
@@ -104,40 +165,9 @@ class Effect extends React.Component {
       )
     }
 
-    else if ((data = asEffect.race(effect.effect))) {
-      nodes = nodes.concat(
-        renderEffectType('race'),
-        this.renderResult(status, result, error, winner)
-      )
-    }
-
-    else if ((data = asEffect.select(effect.effect))) {
-      nodes = nodes.concat(
-        renderEffectType('select'),
-        renderFuncCall(data.selector, data.args),
-        this.renderResult(status, result, error, winner)
-      )
-    }
-
-    else if ((data = asEffect.actionChannel(effect.effect))) {
-      nodes = nodes.concat(
-        renderEffectType('actionChannel'),
-        <SagaValue value={data.action} isIdentifier={true} />,
-        this.renderResult(status, result, error, winner)
-      )
-    }
-
     else if ((data = asEffect.cancelled(effect.effect))) {
       nodes = nodes.concat(
         renderEffectType('cancelled(?)'),
-        this.renderResult(status, result, error, winner)
-      )
-    }
-
-    else if ((data = asEffect.flush(effect.effect))) {
-      nodes = nodes.concat(
-        renderEffectType('flush'),
-        renderFuncCall(data),
         this.renderResult(status, result, error, winner)
       )
     }
