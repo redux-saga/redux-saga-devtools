7876585b0a22f7ebbadfe8fac491171c5d187f10
Alex Bettadapur <alexbettadapur@gmail.com>
2018-07-02 21:37:55 -0700

    Use Immer for the reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -12,6 +12,7 @@ import {
   STATUS_REJECTED,
   STATUS_CANCELLED,
 } from './constants'
+import produce from "immer";
 
 const CHILDREN = Symbol('CHILDREN')
 
@@ -19,9 +20,9 @@ function getPathToEffect(effect, effectsById) {
   let effectId = effect.effectId
   const path = [effectId]
 
-  while(effectId) {
+  while (effectId) {
     effectId = effect.parentEffectId
-    if(effectId) {
+    if (effectId) {
       path.push(effectId)
       effect = effectsById[effectId]
     }
@@ -30,140 +31,123 @@ function getPathToEffect(effect, effectsById) {
 }
 
 export function rootEffectIds(state = [], action) {
-  if(action.type === EFFECT_TRIGGERED && action.effect.root) {
-    return [...state, action.effect.effectId]
-  }
-  return state
+  return produce(state, draft => {
+    if (action.type === EFFECT_TRIGGERED && action.effect.root) {
+      draft.push(action.effect.effectId);
+    }
+  });
 }
 
 export function effectIds(state = [], action) {
-  switch (action.type) {
-    case EFFECT_TRIGGERED:
-      return state.concat(action.effect.effectId)
-    default:
-      return state
-  }
+  return produce(state, draft => {
+    switch (action.type) {
+      case EFFECT_TRIGGERED:
+        draft.push(action.effect.effectId)
+    }
+  });
 }
 
 export function effectsById(state = {}, action) {
-  let effectId, effect, newState
-  switch (action.type) {
-    case EFFECT_TRIGGERED:
-      effect = action.effect
-      effectId = effect.effectId
-      newState = {...state,
-        [effectId]: {
+  let effect, effectId;
+  return produce(state, draft => {
+    switch (action.type) {
+      case EFFECT_TRIGGERED:
+        effect = action.effect
+        effectId = effect.effectId
+        draft[effectId] = {
           ...effect,
           status: STATUS_PENDING,
           start: action.time,
           path: effect.parentEffectId ? getPathToEffect(effect, state) : [effectId],
           [CHILDREN]: []
+        };
+
+        /**
+          ugly  hack. store children along with the effects
+          this shouldn't be accessed by any other outside UI
+          it's only there so the maybeSetRaceWinner could access race's children
+        **/
+        const parent = draft[effect.parentEffectId]
+        if (parent && asEffect.race(parent.effect)) {
+          parent[CHILDREN].push(effect)
         }
-      }
-      /**
-        ugly  hack. store children along with the effects
-        this shouldn't be accessed by any other outside UI
-        it's only there so the maybeSetRaceWinner could access race's children
-      **/
-      const parent = state[effect.parentEffectId]
-      if(parent && asEffect.race(parent.effect)) {
-        parent[CHILDREN].push(effect)
-      }
-      return newState
 
-    case EFFECT_RESOLVED:
-      effectId = action.effectId
-      effect = state[effectId]
-      newState = {
-        ...state,
-        [effectId] : settleEffect(effect, action)
-      }
-      return maybeSetRaceWinner(effect, action.result, newState)
-    case EFFECT_REJECTED:
-      effectId = action.effectId
-      return {
-        ...state,
-        [effectId] : settleEffect(state[effectId], action, true)
-      }
-    case EFFECT_CANCELLED:
-      effectId = action.effectId
-      return {
-        ...state,
-        [effectId] : cancelEffect(state[effectId], action)
-      }
-    default:
-      return state
-  }
+        break;
+      case EFFECT_RESOLVED:
+        effectId = action.effectId
+        effect = draft[effectId]
+        settleEffect(draft[effectId], action);
+        maybeSetRaceWinner(effect, action.result, draft)
+        break;
+      case EFFECT_REJECTED:
+        effectId = action.effectId
+        settleEffect(draft[effectId], action, true)
+        break;
+      case EFFECT_CANCELLED:
+        effectId = action.effectId
+        cancelEffect(draft[effectId], action);
+    }
+  });
 }
 
-function settleEffect(effect, action, isError) {
-  return {
-    ...effect,
-    result: action.result,
-    error: action.error,
-    status: isError ? STATUS_REJECTED : STATUS_RESOLVED,
-    end: action.time,
-    time: action.time - effect.start
-  }
+function settleEffect(draftEffect, action, isError) {
+  draftEffect.result = action.result;
+  draftEffect.error = action.errot;
+  draftEffect.status = isError ? STATUS_REJECTED : STATUS_RESOLVED;
+  draftEffect.end = action.time;
+  draftEffect.time = action.time - action.start;
 }
 
-function cancelEffect(effect, action) {
-  return {
-    ...effect,
-    status: STATUS_CANCELLED,
-    end: action.time,
-    time: action.time - effect.start
-  }
+function cancelEffect(draftEffect, action) {
+  draftEffect.status = STATUS_CANCELLED;
+  draftEffect.end = action.time;
+  draftEffect.time = action.time - draftEffect.start;
 }
 
 export function effectsByParentId(state = {}, action) {
-  if(action.type === EFFECT_TRIGGERED) {
-    const effect = action.effect
-    const parentId = effect.parentEffectId
-    if(parentId) {
-      const siblings = state[parentId]
-      return {...state,
-        [parentId]: !siblings ? [effect.effectId] : [...siblings, effect.effectId]
+  return produce(state, draft => {
+    if (action.type === EFFECT_TRIGGERED) {
+      const effect = action.effect
+      const parentId = effect.parentEffectId
+      if (parentId) {
+        if (!draft[parentId]) {
+          draft[parentId] = [];
+        }
+        draft[parentId].push(effect.effectId);
       }
     }
-  }
-  return state
+  });
 }
 
-function maybeSetRaceWinner(effect, result, state) {
-  if(asEffect.race(effect.effect)) {
+function maybeSetRaceWinner(effect, result, draft) {
+  if (asEffect.race(effect.effect)) {
     const label = Object.keys(result)[0]
     const children = effect[CHILDREN]
     for (var i = 0; i < children.length; i++) {
       const ch = children[i]
-      if(ch.label === label) {
-        // we mutate the state, b/c we know it's already a new generated state from effectsById
-        state[ch.effectId] = {
-          ...state[ch.effectId],
-          winner: true
-        }
-        return state
+      if (ch.label === label) {
+        draft[ch.effectId].winner = true;
+        break;
       }
     }
   }
-  return state
 }
 
 export function dispatchedActions(state = [], monitorAction) {
-  if(monitorAction.type === ACTION_DISPATCHED) {
-    const { id, action, time, isSagaAction } = monitorAction
-    return state.concat({id, action, time, isSagaAction})
-  }
-  return state
+  return produce(state, draft => {
+    if (monitorAction.type === ACTION_DISPATCHED) {
+      const { id, action, time, isSagaAction } = monitorAction
+      draft.push({ id, action, time, isSagaAction })
+    }
+  });
 }
 
 export function sharedRef(state = {}, action) {
-  if(action.type === SET_SHARED_REF) {
-    return {...state,
-      [action.key]: action.sharedRef
+  return produce(state, draft => {
+    if (action.type === SET_SHARED_REF) {
+      draft[action.key] = action.sharedRef;
     }
-  }
-  return state
+  });
 }
 
 export default combineReducers({
